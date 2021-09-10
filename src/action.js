b4a347d23fc5d9a9fdbeef8e5c86f67405f1bb5e
caseymm <casey@bocoup.com>
2021-09-10 10:43:14 -0700

    we'll see

--- src/action.js
+++ src/action.js
@@ -1,5 +1,45 @@
-async function run() {
-  console.log('Hello this is my javascript action running!');
+import AWS from 'aws-sdk';
+import fetch from 'node-fetch';
+
+// Enter copied or downloaded access ID and secret key here
+const ID = 'AKIA3ZAQNP727TDDT353';
+const SECRET = '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';
+
+// The name of the bucket that you have created
+const BUCKET_NAME = 'red-flag-warnings';
+
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET
+});
+
+const uploadFile = (name, data) => {
+  // Read content from the file
+  // const fileContent = fs.readFileSync(fileName);
+
+  // Setting up S3 upload parameters
+  const params = {
+      Bucket: BUCKET_NAME,
+      Key: `${name}.json`, // File name you want to save as in S3
+      Body: JSON.stringify(data)
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function(err, data) {
+      if (err) {
+          throw err;
+      }
+      console.log(`File uploaded successfully. ${data.Location}`);
+  });
+};
+
+async function getLatestRFW(){
+  const rfwUrl = 'https://services9.arcgis.com/RHVPKKiFTONKtxq3/ArcGIS/rest/services/NWS_Watches_Warnings_v1/FeatureServer/9/query?where=Event%3D%27Red+Flag+Warning%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=';
+  const resp = await fetch(rfwUrl);
+  const json = await resp.json()
+  // console.log(json)
+  uploadFile(new Date(), json);
+  uploadFile('latest', json);
 }
 
-run();
+getLatestRFW();
