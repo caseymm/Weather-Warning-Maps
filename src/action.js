0804757fb1cff8553b998ffba4d1293f31326de6
caseymm <casey@bocoup.com>
2021-09-16 11:46:05 -0700

    working out some kinks

--- src/action.js
+++ src/action.js
@@ -8,38 +8,38 @@ const events = {
   'Red Flag Warning': {
     'folder': 'rfw',
     'color': 'e60000',
-    'message': '🚩 New Red Flag Warning 🚩 \n\n A Red Flag Warning has been issued for these areas. Weather events which may result in extreme fire behavior will occur within 24 hours. This is the highest alert. Please use extreme caution.',
-    'alt_message': '🚩 New Red Flag Warning 🚩 \n\n There are currently no Red Flag Warnings.'
+    'message': '🚩 New Red Flag Warning 🚩 \n\nA Red Flag Warning has been issued for these areas. Weather events which may result in extreme fire behavior will occur within 24 hours. This is the highest alert. Please use extreme caution.',
+    'alt_message': '🚩 Cleared: Red Flag Warning 🚩 \n\nThere are currently no Red Flag Warnings.'
   },
   'Fire Weather Watch': {
     'folder': 'fww',
     'color': 'ff6d05',
-    'message': '🏳 New Fire Weather Watch Alert 🏳 \n\n A Fire Weather Watch has been issued for these areas. Weather events which may result in extreme fire behavior could exist in the next 12-72 hours.',
-    'alt_message': '🏳 New Fire Weather Watch Alert 🏳 \n\n There are currently no areas under a Fire Weather Watch Alert.'
+    'message': '🏳 New Fire Weather Watch Alert 🏳 \n\nA Fire Weather Watch has been issued for these areas. Weather events which may result in extreme fire behavior could exist in the next 12-72 hours.',
+    'alt_message': '🏳 Cleared: Fire Weather Watch Alert 🏳 \n\nThere are currently no areas under a Fire Weather Watch Alert.'
   },
   'Severe Thunderstorm Watch': {
     'folder': 'stw',
     'color': '808c9e',
-    'message': '⛈ New Severe Thunderstorm Watch ⛈',
-    'alt_message': '⛈ New Severe Thunderstorm Watch ⛈ \n\n There are currently no areas under advisement for Severe Thunderstorm Watch.'
+    'message': '⛈ New Severe Thunderstorm Watch ⛈ \n\nConditions are favorable for the development of severe thunderstorms in and close to the watch area.',
+    'alt_message': '⛈ Cleared: Severe Thunderstorm Watch ⛈ \n\nThere are currently no areas under advisement for Severe Thunderstorm Watch.'
   },
   'Severe Thunderstorm Warning': {
     'folder': 'stwarning',
     'color': '808c9e',
-    'message': '⛈ New Severe Thunderstorm Warning ⛈',
-    'alt_message': '⛈ New Severe Thunderstorm Warning ⛈ \n\n A thunderstorm producing hail one inch or larger in diameter and/or winds equal or exceed 58 miles an hour has been reported. People in the affected area should seek safe shelter immediately.'
+    'message': '⛈ New Severe Thunderstorm Warning ⛈ \n\nA thunderstorm producing hail one inch or larger in diameter and/or winds equal or exceed 58 miles an hour has been reported. People in the affected area should seek safe shelter immediately.',
+    'alt_message': '⛈ Cleared: Severe Thunderstorm Warning ⛈ \n\nThere are currently no areas under advisement for Severe Thunderstorm Warning.'
   },
   'Flash Flood Watch': {
     'folder': 'ffw',
-    'color': '808c9e',
-    'message': '🌊 New Flash Flood Watch 🌊',
-    'alt_message': '🌊 New Flash Flood Watch 🌊 \n\n Conditions exist or are developing that that are favorable for flash flooding in and close to the watch area.'
+    'color': '00008a',
+    'message': '🌊 New Flash Flood Watch 🌊 \n\nConditions exist or are developing that that are favorable for flash flooding in and close to the watch area.',
+    'alt_message': '🌊 Cleared: Flash Flood Watch 🌊 \n\nThere are currently no areas under advisement for Flash Flooding.'
   },
   'Flash Flood Warning': {
     'folder': 'ffwarning',
-    'color': '808c9e',
-    'message': '🌊 New Flash Flood Warning 🌊',
-    'alt_message': '🌊 New Flash Flood Warning 🌊 \n\n Flash flooding is in progress, imminent, or highly likely. Please seek shelter.'
+    'color': '00008a',
+    'message': '🌊 New Flash Flood Warning 🌊 \n\nFlash flooding is in progress, imminent, or highly likely. Please seek shelter.',
+    'alt_message': '🌊 Cleared: Flash Flood Warning 🌊 \n\nThere are currently no areas under advisement for Flash Flooding.'
   },
 }
 
@@ -108,7 +108,8 @@ async function useTheData(folder, color){
   });
   const page = await context.newPage();
   await page.setViewportSize({ width: 1200, height: 800 });
-  await page.goto(`https://caseymm.github.io/mbx-devour/?url=https://weather-warnings.s3.us-west-1.amazonaws.com/${folder}/latest.json&fill=${color}&fill-opacity=.6`, { waitUntil: 'networkidle0' });
+  await page.goto(`https://caseymm.github.io/mbx-devour/?url=https://weather-warnings.s3.us-west-1.amazonaws.com/${folder}/latest.json&fill=${color}&fill-opacity=.6`, { waitUntil: 'networkidle' });
+  await page.waitForSelector('#hidden', {state: 'hidden'});
   const screenshot = await page.screenshot();
   uploadFile(`${folder}/latest-img`, screenshot, 'png');
   uploadFile(`${folder}/${dateStr}-img`, screenshot, 'png');
@@ -136,15 +137,17 @@ async function getLatestRFW(weatherEvent){
       uploadFile(`${folder}/${dateStr}`, JSON.stringify(json), 'json');
       uploadFile(`${folder}/latest`, JSON.stringify(json), 'json');
 
-      if(json.features.length === 0){
-        message = events[weatherEvent].alt_message;
-      }
       useTheData(folder, color).then(stuff => {
         getObject(BUCKET_NAME, `${folder}/latest-img.png`).then(img => {
           uploadClient.post('media/upload', { media: img }).then(result => {
-            const status = {
+            let status = {
               status: message,
               media_ids: result.media_id_string
+            }
+            if(json.features.length === 0){
+              status = {
+                status: events[weatherEvent].alt_message
+              }
             }
             client.post('statuses/update', status).then(result => {
               console.log('You successfully tweeted this : "' + result.text + '"');
