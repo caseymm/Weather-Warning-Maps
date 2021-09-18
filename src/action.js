74ea5db5b7e054287596025aca50456c3ff302d5
Casey Miller <casey@bocoup.com>
2021-09-17 18:26:50 -0700

    Update action.js

--- src/action.js
+++ src/action.js
@@ -111,14 +111,7 @@ async function useTheData(folder, color){
   const page = await context.newPage();
   await page.setViewportSize({ width: 1200, height: 800 });
   await page.goto(`https://caseymm.github.io/mbx-devour/?url=https://weather-warnings.s3.us-west-1.amazonaws.com/${folder}/latest.json&fill=${color}&fill-opacity=.6`);
-  try{
-    await page.waitForSelector('#hidden', {state: 'attached'});
-  } catch(err){
-    // try again
-    console.log(`${folder} try again`)
-    await page.goto(`https://caseymm.github.io/mbx-devour/?url=https://weather-warnings.s3.us-west-1.amazonaws.com/${folder}/latest.json&fill=${color}&fill-opacity=.6`);
-    await page.waitForSelector('#hidden', {state: 'attached'});
-  }
+  await page.waitForSelector('#hidden', {state: 'attached'});
   const screenshot = await page.screenshot();
   await uploadFile(`${folder}/latest-img`, screenshot, 'png');
   await uploadFile(`${folder}/${dateStr}-img`, screenshot, 'png');
